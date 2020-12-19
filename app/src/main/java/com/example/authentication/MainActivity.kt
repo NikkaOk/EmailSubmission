b6f670539e03ca2c4fd30b6cda925a3878996541
NikkaOk <nika.okropiridze.1@btu.edu.ge>
2020-12-19 05:05:49 +0400

    Email, Password Submission

---
+++ app/src/main/java/com/example/authentication/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputEmail: EditText
+    private lateinit var inputPassword: EditText
+    private lateinit var submitButton: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        inputEmail = findViewById(R.id.signInEmailEditText)
+        inputPassword = findViewById(R.id.signInPasswordEditText)
+        submitButton = findViewById(R.id.submitButton)
+
+        submitButton.setOnClickListener{
+            val email = inputEmail.text.toString()
+            val password = inputPassword.text.toString()
+            
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Empty", Toast.LENGTH_SHORT).show()
+            }else{
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                    task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this, "Submitted successfully", Toast.LENGTH_SHORT).show()
+                        finish()
+                    } else{
+                        Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+        }
+    }
+}
